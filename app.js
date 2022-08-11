e26279956e751a3971630c32eb52d013a815bda9
Sitanshu Khosla <sitanshu.khosla.ext@lvmh.com>
2022-08-11 11:23:12 +0200

    back fix1

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ app.set('port', process.env.PORT || 8000);
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/save', save.save);
-app.post('/validate', validate.validate);
-app.post('/publish', publish.publish);
-app.post('/stop', stop.stop);
-app.post('/execute', execute.execute);
+app.use('/save', save.save);
+app.use('/validate', validate.validate);
+app.use('/publish', publish.publish);
+app.use('/stop', stop.stop);
+app.use('/execute', execute.execute);
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log(`Express server listening on port ${app.get('port')}`);
