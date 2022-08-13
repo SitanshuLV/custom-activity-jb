d5059ea277e2f454d1324943ebd7bc58a236ab8e
Sitanshu Khosla <sitanshu.khosla.ext@lvmh.com>
2022-08-14 01:43:50 +0200

    execute

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -2,12 +2,22 @@ var connection = new Postmonger.Session();
 
 connection.trigger('ready');
 
+connection.on('requestedTriggerEventDefinition', function (eventDefinitionModel) {
+      var eventKey = eventDefinitionModel['eventDefinitionKey'];
+      save(eventKey);
+});
+
 connection.on('initActivity', function( data ) {
-    console.log(data);
     document.getElementById('response').value = JSON.stringify(data, null, 2);
+    payload = data;
 });
 
 connection.on('clickedNext', function() {
     var resp = JSON.parse(document.getElementById('response').value);
-    connection.trigger('updateActivity', resp);
+    var params = {
+        subscriberKey: '{{Contact.key}}',
+        email: '{{Event.' + eventKey + '.Email}}',
+      };
+    payload['arguments'].execute.inArguments = [params];
+    connection.trigger('updateActivity', payload);
 });
